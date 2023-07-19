5f709b439043cf0dd3c185aa280e7fe93f2e4ce9
ntorionbearstudio <53612278+ntorionbearstudio@users.noreply.github.com>
2023-07-19 11:47:57 +0200

    Update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 export { Box } from './box/box.component';
 export { BoxProps } from './box/box.type';
+export { ScrollBox } from './scrollbox/scrollbox.component';
+export { ScrollBoxProps } from './scrollbox/scrollbox.type';
+export { Flex } from './flex/flex.component';
+export { FlexProps } from './flex/flex.type';
 export { Text } from './text/text.component';
 export { TextProps } from './text/text.type';
