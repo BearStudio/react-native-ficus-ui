7525532179e3c8e479192134f8b6c0bdbccac54b
ntorionbearstudio <53612278+ntorionbearstudio@users.noreply.github.com>
2023-07-22 15:27:55 +0200

    Fix

--- src/components/list/list.spec.tsx
+++ src/components/list/list.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 
 import { Text } from '../text/text.component';
 import { List } from './list.component';
