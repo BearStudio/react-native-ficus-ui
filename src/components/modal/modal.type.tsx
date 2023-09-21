da8c0a154261b9e58fa2c860008769828fc4fb37
Nicolas Torion <nicolas.torion@bearstudio.fr>
2023-09-21 17:23:54 +0200

    Fix on Modal component

--- src/components/modal/modal.type.tsx
+++ src/components/modal/modal.type.tsx
@@ -10,7 +10,7 @@ import {
 } from '../../types';
 
 export interface ModalProps
-  extends RNModalProps,
+  extends Partial<RNModalProps>,
     BorderPropsType,
     SpacingPropsType,
     BorderRadiusPropsType,
