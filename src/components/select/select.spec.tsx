0b2d3c87bd96b1560682cf3117df99ad7d3e7d78
deelan <deelanpsm@gmail.com>
2024-01-14 11:04:00 +0100

    Unit tests on Select component

--- src/components/select/select.spec.tsx
+++ src/components/select/select.spec.tsx
@@ -1,26 +1,32 @@
-import { cleanup, fireEvent, render, screen } from '@testing-library/react-native';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react-native';
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Button } from '../button/button.component';
 import { Select } from './select.component';
+import { Option } from './select.option.component';
 import { SelectRef } from './select.type';
 
 afterEach(cleanup);
 
-describe('Test react-select component', () => {
+describe('Select component', () => {
   const mockedOptions = [
     { label: 'Mocked option 1', value: 'mocked-option-1' },
     { label: 'Mocked option 2', value: 'mocked-option-2' },
     { label: 'Mocked option 3', value: 'mocked-option-3' },
   ];
 
-  it('Should render when button is clicked without errors', async () => {
+  it('should render when button is clicked without errors', async () => {
     const selectRef = React.createRef<SelectRef>();
 
     const selectId = 'select-id';
     const buttonId = 'button-id';
 
-   render(
+    render(
       <View>
         <Button testID={buttonId} onPress={() => selectRef.current?.open()}>
           Select button
@@ -37,21 +43,21 @@ describe('Test react-select component', () => {
     );
 
     const select = screen.queryByTestId(selectId);
-    const button = screen.queryByTestId(buttonId);
+    const button = screen.getByTestId(buttonId);
 
-    expect(select).toBeDefined();
-    expect(select.props).toHaveProperty('visible');
-    expect(select.props.visible).toBeFalsy();
+    expect(select).not.toBeNull();
+    expect(select?.props).toHaveProperty('visible');
+    expect(select?.props.visible).toBeFalsy();
 
     fireEvent.press(button);
 
-    expect(select.props.visible).toBeTruthy();
+    expect(select?.props.visible).toBeTruthy();
   });
 
-  it('closes the Select modal when an option is selected', async () => {
+  it('should handle single selection', async () => {
     const selectRef = React.createRef<SelectRef>();
+    const selectFn = jest.fn();
 
-    const selectId = 'select-id';
     const buttonId = 'button-id';
 
     render(
@@ -61,27 +67,111 @@ describe('Test react-select component', () => {
         </Button>
 
         <Select
-          testID={selectId}
+          onSelect={selectFn}
           title={'Select an option'}
           ref={selectRef}
           data={mockedOptions}
-          value={mockedOptions.at(0)}
-          renderItem={(item, index) => (
-            <Text>s</Text>
+          value={mockedOptions[0]}
+          renderItem={(item) => (
+            <Option value={item} py="md" px="xl">
+              <Text>{item.label}</Text>
+            </Option>
           )}
         />
       </View>
     );
 
-    const select = screen.queryByTestId(selectId);
-    const button = screen.queryByTestId(buttonId);
-
-    expect(select).toBeDefined();
-    expect(select.props).toHaveProperty('visible');
-    expect(select.props.visible).toBeFalsy();
+    const button = screen.getByTestId(buttonId);
 
     fireEvent.press(button);
+    const option = screen.getByText('Mocked option 2');
+    fireEvent.press(option);
+    expect(selectFn).toHaveBeenLastCalledWith(mockedOptions[1]);
+  });
 
-    expect(select.props.visible).toBeTruthy();
+  it('should handle single selection with submit prop', async () => {
+    const selectRef = React.createRef<SelectRef>();
+    const selectFn = jest.fn();
+
+    const buttonId = 'button-id';
+
+    render(
+      <View>
+        <Button testID={buttonId} onPress={() => selectRef.current?.open()}>
+          Select button
+        </Button>
+
+        <Select
+          submit
+          onSelect={selectFn}
+          ref={selectRef}
+          value={1}
+          title="This is your title"
+          mt="md"
+          pb="2xl"
+          message="This is the long message used to set some context"
+          data={[1, 2, 3, 4, 5, 6]}
+          renderItem={(item) => (
+            <Option value={item} py="md" px="xl">
+              <Text>{item}</Text>
+            </Option>
+          )}
+        />
+      </View>
+    );
+
+    const button = screen.getByTestId(buttonId);
+
+    fireEvent.press(button);
+    const option = screen.getByText('2');
+    const submit = screen.getByText('Submit');
+
+    fireEvent.press(option);
+    expect(selectFn).not.toHaveBeenCalled();
+    fireEvent.press(submit);
+    expect(selectFn).toHaveBeenCalledWith(2);
+  });
+
+  it('should handle multiple selection', async () => {
+    const selectRef = React.createRef<SelectRef>();
+    const selectFn = jest.fn();
+    const buttonId = 'button-id';
+
+    render(
+      <View>
+        <Button testID={buttonId} onPress={() => selectRef.current?.open()}>
+          Select button
+        </Button>
+
+        <Select
+          isMultiple
+          onSelect={selectFn}
+          ref={selectRef}
+          value={[]}
+          title="This is your title"
+          mt="md"
+          pb="2xl"
+          message="This is the long message used to set some context"
+          data={[1, 2, 3, 4, 5, 6]}
+          renderItem={(item) => (
+            <Option value={item} py="md" px="xl">
+              <Text>{item}</Text>
+            </Option>
+          )}
+        />
+      </View>
+    );
+
+    const button = screen.getByTestId(buttonId);
+
+    fireEvent.press(button);
+    const option1 = screen.getByText('1');
+    const option3 = screen.getByText('3');
+
+    const submit = screen.getByText('Submit');
+    fireEvent.press(option1);
+    fireEvent.press(option3);
+    fireEvent.press(submit);
+    expect(selectFn).toHaveBeenLastCalledWith([1, 3]);
   });
 });
