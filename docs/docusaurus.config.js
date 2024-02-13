8ebf046ac351d2902a67d1417aff78eee966edb5
houssembaltii <houssem@bearstudio.fr>
2024-02-13 17:54:28 +0100

    chore : rename files

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('./src/theme/LightCustomTheme.js');
-const darkCodeTheme = require('./src/theme/DarkCustomTheme.js');
+const lightCodeTheme = require('./src/theme/lightCustomTheme.js')
+const darkCodeTheme = require('./src/theme/darkCustomTheme.js')
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'React Native Ficus UI',
