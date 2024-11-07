dee9ecb2314e341b565f9b60c76b4c32c89056db
Livio Gamassia <livio@appik-studio.ch>
2024-11-07 23:20:09 +0100

    fix: props can be undefined making stacks crash

--- src/components/stack/stack.component.tsx
+++ src/components/stack/stack.component.tsx
@@ -104,7 +104,7 @@ const Stack: React.FunctionComponent<StackProps> = (incomingProps) => {
             <React.Fragment key={`stack-${index}`}>
               {!!item && (
                 <React.Fragment>
-                  <RNView style={{ zIndex: item.props.zIndex }}>{item}</RNView>
+                  <RNView style={{ zIndex: item.props?.zIndex }}>{item}</RNView>
                   {index !== children.length - 1 ? (
                     <RNView style={computedStyle.stackSpacing} />
                   ) : null}
