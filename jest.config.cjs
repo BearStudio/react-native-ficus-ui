060577826378b59a4acdf7c54968022b32b31775
Noé TATOUD <noe@bearstudio.fr>
2025-02-13 17:48:14 +0100

    chore(code-quality): improve ts, eslint, prettier and tests config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   preset: 'react-native',
-  // rootDir: __dirname,
   modulePaths: ['./src'],
-  setupFilesAfterEnv: ['./jest-setup.ts'],
   transformIgnorePatterns: ['node_modules/(?!.*react-native).*'],
   testPathIgnorePatterns: [
     '/lib/typescript/.*\\.test\\.d\\.ts$',
