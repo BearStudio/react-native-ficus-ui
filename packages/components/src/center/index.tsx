eb60dbc86d114c34568d5db404a8bccb4e62a76c
harry-vu <vuhieuvpz@gmail.com>
2025-02-19 18:19:36 +0700

    chore: remove unnesscaryproperties absolute center

--- packages/components/src/center/index.tsx
+++ packages/components/src/center/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import { NativeFicusProps, ficus, forwardRef } from '../system';
+import { NativeFicusProps, ficus } from '../system';
 
 export interface CenterProps extends NativeFicusProps<'View'> {}
 
@@ -11,37 +9,3 @@ export const Center = ficus('View', {
     justifyContent: 'center',
   },
 });
-
-export interface AbsoluteCenterProps extends NativeFicusProps<'View'> {
-  axis?: 'horizontal' | 'vertical' | 'both';
-}
-
-const centerStyles = {
-  horizontal: {
-    insetStart: '50%',
-    transform: 'translateX(-50%)',
-  },
-  vertical: {
-    top: '50%',
-    transform: 'translateY(-50%)',
-  },
-  both: {
-    insetStart: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
-
-export const AbsoluteCenter = forwardRef<AbsoluteCenterProps, 'View'>(
-  function AbsoluteCenter(props, ref) {
-    const { axis = 'both', ...rest } = props;
-    return (
-      <ficus.View
-        ref={ref}
-        __styles={centerStyles[axis]}
-        {...rest}
-        position="absolute"
-      />
-    );
-  }
-);
