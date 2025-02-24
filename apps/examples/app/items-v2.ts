cb8d38394ff113516bb8b7635b37d719b88a5dd5
harry-vu <vuhieuvpz@gmail.com>
2025-02-24 14:17:14 +0700

    feat(components/safeAreaBox): add component, add to example, add to doc, add test

--- apps/examples/app/items-v2.ts
+++ apps/examples/app/items-v2.ts
@@ -1,6 +1,7 @@
 import { ComponentType } from 'react';
 import BoxComponent from './components-v2/Box';
 import TextComponent from '@/app/components-v2/Text';
+import SafeAreaBoxComponent from '@/app/components-v2/SafeAreaBox';
 
 type ExampleComponentType = {
   onScreenName: string;
@@ -12,4 +13,5 @@ export const components: ExampleComponentType[] = [
 
   { navigationPath: 'Box', onScreenName: 'Box', component: BoxComponent },
   { navigationPath: 'Text', onScreenName: 'Text', component: TextComponent },
+  { navigationPath: 'SafeAreaBox', onScreenName: 'SafeAreaBox', component: SafeAreaBoxComponent },
 ];
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,4 @@
 export * from './box';
 export * from './text';
+export * from './safeAreaBox';
 export { ThemeProvider } from '@ficus-ui/theme';
