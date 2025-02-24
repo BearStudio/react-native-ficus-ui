87a096b8eed0d32dc0f9e9df4ad78cd7872328a5
harry-vu <vuhieuvpz@gmail.com>
2025-02-24 18:22:16 +0700

    chore: fixed conflict

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,9 +1,6 @@
 export * from './box';
-<<<<<<< HEAD
 export * from './text';
 export * from './safe-area-box';
-=======
 export * from './badge';
 
->>>>>>> upstream/next
 export { ThemeProvider } from '@ficus-ui/theme';
