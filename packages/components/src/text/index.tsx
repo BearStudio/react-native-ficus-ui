8bfc83628cab11a2147cbb6608eaa6ba3c24e939
harry-vu <vuhieuvpz@gmail.com>
2025-02-24 18:19:30 +0700

    chore: remove unnesscary code and remove themeprovider

--- packages/components/src/text/index.tsx
+++ packages/components/src/text/index.tsx
@@ -1,9 +1,4 @@
-import {
-  TextStyleProps,
-  ThemingProps,
-  omitThemingProps,
-  splitTextProps,
-} from '@ficus-ui/style-system';
+import { omitThemingProps } from '@ficus-ui/style-system';
 
 import {
   type NativeFicusProps,
@@ -12,18 +7,13 @@ import {
   useStyleConfig,
 } from '../system';
 
-export interface TextProps
-  extends NativeFicusProps<'Text'>,
-    TextStyleProps,
-    ThemingProps<'Text'> {}
+export interface TextProps extends NativeFicusProps<'Text'> {}
 export const Text = forwardRef<TextProps, 'Text'>(function Text(props, ref) {
   const styles = useStyleConfig('Text', props);
   const { children, ...rest } = omitThemingProps(props);
 
-  const [textStyles] = splitTextProps(styles);
-
   return (
-    <ficus.Text ref={ref} {...rest} __styles={textStyles}>
+    <ficus.Text ref={ref} {...rest} __styles={styles}>
       {children}
     </ficus.Text>
   );
