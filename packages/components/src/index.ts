5789728d766c4cd6a42b276f4385ea478cfbe85d
harry-vu <vuhieuvpz@gmail.com>
2025-02-24 15:29:23 +0700

    chore: resolved feedbacks about file and folder name

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,5 @@
 export * from './box';
 export * from './text';
 export * from './safeAreaBox';
-export * from './scrollBox';
+export * from './scroll-box';
 export { ThemeProvider } from '@ficus-ui/theme';
