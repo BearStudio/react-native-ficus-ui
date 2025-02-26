b9f65a634e3d2089221d77836b284d67ef908c2f
harry-vu <vuhieuvpz@gmail.com>
2025-02-26 17:03:54 +0700

    test: need to check ficus factory

--- apps/examples/app/components-v2/Spinner.tsx
+++ apps/examples/app/components-v2/Spinner.tsx
@@ -1,4 +1,5 @@
-import { Box, SafeAreaBox, Spinner, Text } from "@ficus-ui/native";
+import ExampleSection from "@/src/ExampleSection";
+import { Box, SafeAreaBox, Spinner, Spinner2, Text } from "@ficus-ui/native";
 
 
 const SpinnerComponent = () => {
@@ -7,11 +8,20 @@ const SpinnerComponent = () => {
       <Text mx="xl" fontSize="4xl">
         Spinner component
       </Text>
+      <ExampleSection name="spinner original">
+      <Box mx="xl" mt="lg">
+        <Spinner2 />
+        <Spinner2 color="red" size="small" />
+        <Spinner2 color="black" size="large" />
+      </Box>
+      </ExampleSection>
+      <ExampleSection name="spinner ficus">
       <Box mx="xl" mt="lg">
         <Spinner />
         <Spinner color="red" size="small" />
         <Spinner color="black" size="large" />
       </Box>
+      </ExampleSection>
     </SafeAreaBox>
   );
 };
--- packages/components/src/spinner/index.tsx
+++ packages/components/src/spinner/index.tsx
@@ -1,11 +1,11 @@
 import { ActivityIndicator } from 'react-native';
 
-import { type NativeFicusProps } from '../system';
+import { type NativeFicusProps, ficus } from '../system';
 
 export interface SpinnerProps extends NativeFicusProps<'ActivityIndicator'> {}
 
-// export const Spinner = ficus('ActivityIndicator'); // something wrong with ficus making color return undefined but size still working correctly
-
-export const Spinner = (props: SpinnerProps) => {
+export const Spinner = ficus('ActivityIndicator'); // something wrong with ficus making color return undefined but size still working correctly
+// color return undefined can be check in spinner.spec.tsx
+export const Spinner2 = (props: SpinnerProps) => {
   return <ActivityIndicator {...props} />; // using normal ActivityIndicator instead of ficus until found out why color isnt working
 };
