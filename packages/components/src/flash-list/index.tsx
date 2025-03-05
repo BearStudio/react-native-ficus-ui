ea0a0b01729b63adfe71ab7cd695440b118779ca
Noé TATOUD <noe@bearstudio.fr>
2025-03-05 20:30:13 +0100

    fix(components/flash-list): Add flex 1 to base styles

--- packages/components/src/flash-list/index.tsx
+++ packages/components/src/flash-list/index.tsx
@@ -1,4 +1,9 @@
 import { type NativeFicusProps, ficus } from '../system';
 
 export interface FlashListProps extends NativeFicusProps<'FlashList'> {}
-export const FlashList = ficus('FlashList');
+
+export const FlashList = ficus('FlashList', {
+  baseStyle: {
+    flex: 1,
+  },
+});
