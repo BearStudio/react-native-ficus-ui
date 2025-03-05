0713e67d48d84af2346adb12a0b46f833f013e97
Zero6d <100817464+Zero6d@users.noreply.github.com>
2025-03-06 01:49:52 +0700

    feat(v2): Icon component (#80)
    * feat(components/icon): add component, add to example, add to doc, add test
    
    * fix(components/icon): Clean code
    
    * fix(tests): icon and sliders tests fixs
    
    * chore(theme): Remove theme service file
    
    ---------
    
    Co-authored-by: harry-vu <vuhieuvpz@gmail.com>
    Co-authored-by: Noé TATOUD <noe@bearstudio.fr>

---
+++ apps/examples/app/components-v2/Icon.tsx
@@ -0,0 +1,49 @@
+import ExampleSection from "@/src/ExampleSection";
+import { HStack, Icon, SafeAreaBox, ScrollBox, Text } from "@ficus-ui/native";
+
+
+const IconComponent = () => {
+  return (
+    <SafeAreaBox>
+      <Text mx="xl" fontSize="4xl">
+        Icon component
+      </Text>
+      <ScrollBox>
+        <ExampleSection name="default">
+          <HStack spacing={10}>
+            <Icon name="accessibility" color="blue.700" size="6xl" iconSet="Ionicons" />
+            <Icon
+              name="star"
+              color="yellow.700"
+              size="6xl"
+              iconSet="Ionicons"
+            />
+          </HStack>
+        </ExampleSection>
+        <ExampleSection name="sizes">
+          <HStack spacing={10}>
+            <Icon name="alarm" color="red.500" size="md"  />
+            <Icon name="analytics" color="teal.500" size="xl" />
+            <Icon name="heart" color="red.500" size="2xl" />
+            <Icon name="brush" color="blue.700" size="6xl" />
+          </HStack>
+        </ExampleSection>
+        <ExampleSection name="back button">
+          <HStack spacing={10}>
+            <Icon
+              bg="yellow.400"
+              p={15}
+              borderRadius="full"
+              name="arrow-back"
+              color="yellow.700"
+              size="2xl"
+              iconSet="Ionicons"
+            />
+          </HStack>
+        </ExampleSection>
+      </ScrollBox>
+    </SafeAreaBox>
+  );
+};
+
+export default IconComponent;
