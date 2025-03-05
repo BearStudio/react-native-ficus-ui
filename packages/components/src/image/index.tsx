b598426c83777f43de0a867c9fb52e2a7af542aa
Noé TATOUD <noe@bearstudio.fr>
2025-03-05 11:03:21 +0100

    fix(components/image): useImage hook + spread source prop

--- packages/components/src/image/index.tsx
+++ packages/components/src/image/index.tsx
@@ -1,12 +1,27 @@
 import { type NativeFicusProps, ficus, forwardRef } from '../system';
 
 interface ImageOptions {
+  /**
+   * The React native `source.uri` property
+   */
   src?: string;
 }
 
 export interface ImageProps extends NativeFicusProps<'Image'>, ImageOptions {}
 
 export const Image = forwardRef<ImageProps, 'Image'>((props, ref) => {
-  const { src, ...rest } = props;
-  return <ficus.Image ref={ref} {...rest} source={{ uri: src }} />;
+  const { src, source: sourceProp, ...rest } = props;
+
+  const getSourceProp = () => {
+    if (typeof sourceProp === 'number') {
+      return sourceProp;
+    }
+
+    return {
+      uri: src,
+      ...sourceProp,
+    };
+  };
+
+  return <ficus.Image ref={ref} {...rest} source={getSourceProp()} />;
 });
