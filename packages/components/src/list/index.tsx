7300f3da0cfcbf47fcad11a6ab6e6009baffdec4
Noé TATOUD <noe@bearstudio.fr>
2025-03-05 20:02:17 +0100

    feat(components/list): Add flex 1 default style

--- packages/components/src/list/index.tsx
+++ packages/components/src/list/index.tsx
@@ -2,4 +2,8 @@ import { type NativeFicusProps, ficus } from '../system';
 
 export interface ListProps extends NativeFicusProps<'Flatlist'> {}
 
-export const List = ficus('Flatlist');
+export const List = ficus('Flatlist', {
+  baseStyle: {
+    flex: 1,
+  },
+});
