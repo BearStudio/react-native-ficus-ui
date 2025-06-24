6753b74582b77e153e0e258c498614335ab15604
Nicolas Torion <nicolas.torion@bearstudio.fr>
2025-06-24 09:38:44 +0200

    Update toast dark mode

--- packages/react-native-ficus-ui/src/hooks/toast/useToast.tsx
+++ packages/react-native-ficus-ui/src/hooks/toast/useToast.tsx
@@ -1,8 +1,17 @@
 import Toast from 'react-native-toast-message';
 
+import { ThemeToastProps } from '../../theme/theme.default';
+import { useColorMode } from '../color-mode/useColorMode';
+
 const useToast = () => {
   const { show, hide } = Toast;
-  return { show, hide, Toast };
+
+  const { colorMode } = useColorMode();
+
+  const showToast = (props: ThemeToastProps) =>
+    show({ props: { colorMode }, ...props });
+
+  return { show: showToast, hide, Toast };
 };
 
 export { useToast };
--- packages/react-native-ficus-ui/src/theme/theme.default.tsx
+++ packages/react-native-ficus-ui/src/theme/theme.default.tsx
@@ -3,10 +3,16 @@ import {
   BaseToast,
   BaseToastProps,
   ToastPosition,
+  ToastProps,
 } from 'react-native-toast-message';
 
+import { Dict } from '../style-system';
 import { components } from './components';
 import { foundations } from './foundations';
+
+export interface ThemeToastProps extends BaseToastProps, ToastProps {
+  props?: Dict;
+}
 
 export const theme = {
   components,
@@ -15,17 +21,81 @@ export const theme = {
   toastProps: {
     position: 'bottom' as ToastPosition,
     config: {
-      success: (props: BaseToastProps) => (
-        <BaseToast style={{ borderLeftColor: '#38A169' }} {...props} />
+      success: (toastProps: ThemeToastProps) => (
+        <BaseToast
+          style={{
+            borderLeftColor:
+              toastProps?.props?.colorMode === 'dark' ? '#68D391' : '#38A169',
+            backgroundColor:
+              toastProps?.props?.colorMode === 'dark' ? '#4A5568' : 'white',
+          }}
+          text1Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#171923',
+          }}
+          text2Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#1A202C',
+          }}
+          {...toastProps}
+        />
       ),
-      error: (props: BaseToastProps) => (
-        <BaseToast style={{ borderLeftColor: '#E53E3E' }} {...props} />
+      error: (toastProps: ThemeToastProps) => (
+        <BaseToast
+          style={{
+            borderLeftColor:
+              toastProps?.props?.colorMode === 'dark' ? '#FC8181' : '#E53E3E',
+            backgroundColor:
+              toastProps?.props?.colorMode === 'dark' ? '#4A5568' : 'white',
+          }}
+          text1Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#171923',
+          }}
+          text2Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#1A202C',
+          }}
+          {...toastProps}
+        />
       ),
-      warning: (props: BaseToastProps) => (
-        <BaseToast style={{ borderLeftColor: '#DD6B20' }} {...props} />
+      warning: (toastProps: ThemeToastProps) => (
+        <BaseToast
+          style={{
+            borderLeftColor:
+              toastProps?.props?.colorMode === 'dark' ? '#F6AD55' : '#DD6B20',
+            backgroundColor:
+              toastProps?.props?.colorMode === 'dark' ? '#4A5568' : 'white',
+          }}
+          text1Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#171923',
+          }}
+          text2Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#1A202C',
+          }}
+          {...toastProps}
+        />
       ),
-      info: (props: BaseToastProps) => (
-        <BaseToast style={{ borderLeftColor: '#3182ce' }} {...props} />
+      info: (toastProps: ThemeToastProps) => (
+        <BaseToast
+          style={{
+            borderLeftColor:
+              toastProps?.props?.colorMode === 'dark' ? '#63b3ed' : '#3182ce',
+            backgroundColor:
+              toastProps?.props?.colorMode === 'dark' ? '#4A5568' : 'white',
+          }}
+          text1Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#171923',
+          }}
+          text2Style={{
+            color:
+              toastProps?.props?.colorMode === 'dark' ? 'white' : '#1A202C',
+          }}
+          {...toastProps}
+        />
       ),
     },
   },
