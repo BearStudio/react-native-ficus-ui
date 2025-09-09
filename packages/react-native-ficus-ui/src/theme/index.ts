89bb56fd155adc85ea3d6541fa5780ecb9b63c97
Héloïse Guillaume <139237256+heloise-gllm@users.noreply.github.com>
2025-09-09 14:56:21 +0200

    fix: window_width, StackProps, IconSet imports (#107)

--- packages/react-native-ficus-ui/src/theme/index.ts
+++ packages/react-native-ficus-ui/src/theme/index.ts
@@ -2,3 +2,4 @@ export { theme, type Theme } from './theme.default';
 export { ThemeProvider, type ThemeProviderProps } from './provider';
 export * from './hook';
 export * from './utils';
+export * from './utilities';
