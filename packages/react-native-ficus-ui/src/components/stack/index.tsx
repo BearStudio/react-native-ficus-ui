3adbf3e4e7907a09942d0589fdb6f059e15fb7ab
Nicolas Torion <nicolas.torion@bearstudio.fr>
2025-09-10 12:08:33 +0200

    Fix CI

--- packages/react-native-ficus-ui/src/components/stack/index.tsx
+++ packages/react-native-ficus-ui/src/components/stack/index.tsx
@@ -1,4 +1,5 @@
 export { HStack } from './hstack';
-export { Stack, StackProps } from './stack';
+export { Stack } from './stack';
+export type { StackProps } from './stack';
 export { StackDivider } from './stack-divider';
 export { VStack } from './vstack';
