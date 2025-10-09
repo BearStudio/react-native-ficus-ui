4e0d3f120116fbd25d9956f9fb0640246096b2f0
hguillaume <heloise.guillaume@bearstudio.fr>
2025-10-09 17:55:14 +0200

    fix: custom render with button

--- apps/examples/app/components/Radio.tsx
+++ apps/examples/app/components/Radio.tsx
@@ -1,9 +1,21 @@
-import { Badge, Box, HStack, VStack, Radio, RadioGroup, Text, SafeAreaBox, useColorModeValue } from "react-native-ficus-ui";
-import ExampleSection from "@/src/ExampleSection";
+import {
+  Badge,
+  Box,
+  Button,
+  HStack,
+  Radio,
+  RadioGroup,
+  SafeAreaBox,
+  Text,
+  VStack,
+  useColorModeValue,
+} from 'react-native-ficus-ui';
+
+import ExampleSection from '@/src/ExampleSection';
 
 const RadioComponent = () => {
   return (
-    <SafeAreaBox flex={1} bg={useColorModeValue("gray.100", "gray.800")}>
+    <SafeAreaBox flex={1} bg={useColorModeValue('gray.100', 'gray.800')}>
       <Text mx="xl" fontSize="4xl">
         Radio component
       </Text>
@@ -19,24 +31,16 @@ const RadioComponent = () => {
           <Radio value={4} isDisabled>
             Label
           </Radio>
-          <Radio value={5}>
-            Label
-          </Radio>
+          <Radio value={5}>Label</Radio>
         </Box>
       </ExampleSection>
 
       <ExampleSection name="Simple radio group">
         <RadioGroup colorScheme="red">
           <VStack spacing="sm">
-            <Radio value={1}>
-              Option 1
-            </Radio>
-            <Radio value={2}>
-              Option 2
-            </Radio>
-            <Radio value={3}>
-              Option 3
-            </Radio>
+            <Radio value={1}>Option 1</Radio>
+            <Radio value={2}>Option 2</Radio>
+            <Radio value={3}>Option 3</Radio>
           </VStack>
         </RadioGroup>
       </ExampleSection>
@@ -63,21 +67,41 @@ const RadioComponent = () => {
       <ExampleSection name="Custom radio">
         <RadioGroup colorScheme="red">
           <HStack spacing="md">
-            {["Option 1", "Option 2", "Option 3"].map((item) => (
-                <Radio value={item} key={`radio-option-${item}`}>
-                  {({ isChecked }) => (
-                    <Badge
-                      variant={isChecked ? "solid" : "subtle"}
-                      colorScheme="pink"
-                      fontSize="xl"
-                      px="lg"
-                      py="lg"
-                      borderRadius="full"
-                    >
-                      {item}
-                    </Badge>
-                  )}
-                </Radio>
+            {['Option 1', 'Option 2', 'Option 3'].map((item) => (
+              <Radio value={item} key={`radio-option-${item}`}>
+                {({ isChecked }) => (
+                  <Badge
+                    variant={isChecked ? 'solid' : 'subtle'}
+                    colorScheme="pink"
+                    fontSize="xl"
+                    px="lg"
+                    py="lg"
+                    borderRadius="full"
+                  >
+                    {item}
+                  </Badge>
+                )}
+              </Radio>
+            ))}
+          </HStack>
+        </RadioGroup>
+      </ExampleSection>
+      <ExampleSection name="Custom radio with button">
+        <RadioGroup colorScheme="red">
+          <HStack spacing="md">
+            {['Option 1', 'Option 2', 'Option 3'].map((item) => (
+              <Radio value={item} key={`radio-option-${item}`}>
+                {({ isChecked, onToggle }) => (
+                  <Button
+                    bg={isChecked ? 'pink.500' : 'pink.200'}
+                    fontSize="xl"
+                    borderRadius="full"
+                    onPress={onToggle}
+                  >
+                    {item}
+                  </Button>
+                )}
+              </Radio>
             ))}
           </HStack>
         </RadioGroup>
